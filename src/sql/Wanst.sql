a0e97d09fdd08eab42fe8e42541fdc9ee4456fbe
박상민 <68181461+akdrhtn1@users.noreply.github.com>
2022-01-11 17:13:33 +0900

    Update Wanst.sql

--- src/sql/Wanst.sql
+++ src/sql/Wanst.sql
@@ -1,5 +1,4 @@
--- ÀÖ¾ú´Âµ¥¿ä ¾ø¾ú½À´Ï´Ù.!! 
-
+-- ìžˆì—ˆëŠ”ë°ìš” ì—†ì—ˆìŠµë‹ˆë‹¤.
 SELECT * FROM ANIMAL_INS;
 
 INSERT INTO ANIMAL_INS VALUES('A350276','CAT',to_date('2017/08/13 13:50:00','yyyy-mm-dd hh24:mi:ss'),'Normal','Jewel','Spayed Female');
